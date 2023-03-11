b00ef17a548e41c99042d7ed5cb18a1442f807cd
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-11 17:59:51 +0530

    chore: poppins font setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	subsets: ['latin'],
+	style: ['italic', 'normal'],
+	variable: '--font-poppins',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${poppins.variable} font-primary`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
